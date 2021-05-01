eb3092ec50c2d487e6152dc091bf3dc86976d7d7
dasanub_08 <anubrata.life08@gmail.com>
2021-05-01 11:57:16 +0530

    commit for search bar implementaation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState([]);
+  const getDataFromAPI = () => {
+    fetch('https://swapi.dev/api/people/?search=sky').then((response) => {
+      return response.json()
+    }).then((response) => {
+      console.log(response.results)
+      for (var i = 0; i < response.results.length; i++) {
+        data.push(response.results[i].name)
+      }
+      setData(data);
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" placeholder="Search..." onChange={(event) => setSearchTerm(event.target.value)} />
+      {data.filter((val) => {
+        if (searchTerm === "") {
+          return "";
+        } else if (val.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+          return val;
+        }
+      }).map((val, key) => {
+        return (
+          <div classname="user" key={key}>
+            <p>{val.name}</p>
+          </div>
+        );
+      })}
     </div>
+
   );
 }
 
